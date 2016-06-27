87cb6c0d9af66591d218bd53684d58e15601daf4
Craig Rodrigues <rodrigc@FreeBSD.org>
2016-06-26 21:56:03 -0700

    Do not overlap lists of scripts

--- twisted/python/dist.py
+++ twisted/python/dist.py
@@ -219,9 +219,7 @@ def getConsoleScripts():
                 "conch = twisted.conch.scripts.conch:run",
                 "mailmail = twisted.mail.scripts.mailmail:run",
                 "pyhtmlizer = twisted.scripts.htmlizer:run",
-                "tkconch = twisted.conch.scripts.tkconch:run",
-                "trial = twisted.scripts.trial:run",
-                "twistd = twisted.scripts.twistd:run"
+                "tkconch = twisted.conch.scripts.tkconch:run"
               ]
     portedToPython3Scripts = [ "trial = twisted.scripts.trial:run",
                                "twistd = twisted.scripts.twistd:run"
@@ -229,7 +227,7 @@ def getConsoleScripts():
     if _PY3:
         return portedToPython3Scripts
     else:
-        return scripts
+        return scripts + portedToPython3Scripts
 
 
 ## Helpers and distutil tweaks
